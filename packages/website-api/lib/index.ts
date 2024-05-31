6a48d6ded86886246f2586a7c1d6880e7c3b35f4
imtaotao <dwchentao@163.com>
2024-05-31 23:52:39 +0800

    refactor: refactor server && modify tsconfig.json

--- packages/website-api/lib/index.ts
+++ packages/website-api/lib/index.ts
@@ -1,12 +1,21 @@
 import path from "node:path";
-import { apiApp } from "./apis";
 import { lits } from "@website/lits";
+import { isDev } from "@website/utils/node";
+import { apiApp } from "./apis";
+
+const devPort = 8000;
+const staticDir = path.resolve(__dirname, "../../website-web/dist");
 
 lits({
   apiApp,
-  staticDir: path.resolve(__dirname, "../../website-web/dist"),
+  devPort,
+  staticDir,
   ssl: {
     key: process.env.SSL_KEY as string,
     cert: process.env.SSL_CERT as string,
   },
+}).then(() => {
+  isDev
+    ? console.log(`http2 server is running: https://localhost:${devPort}`)
+    : console.log("http2 server is running.");
 });
--- packages/website-lits/lib/index.ts
+++ packages/website-lits/lib/index.ts
@@ -7,15 +7,16 @@ import mount from "koa-mount";
 import sslify from "koa-sslify";
 import koaStatic from "koa-static";
 import compress from "koa-compress";
-import { isDev, SSL } from "@website/utils/node";
+import { isDev } from "@website/utils/node";
 import { createDeployApp } from "./deploy";
-import { defense } from "./middlewares/defense";
+import { defense } from "@/middlewares/defense";
 import { redirectHttps } from "./middlewares/redirectHttps";
 
 let started = false;
 
 export interface Options {
   apiApp: Koa;
+  devPort: number;
   staticDir: string;
   ssl: {
     key: string;
@@ -23,41 +24,43 @@ export interface Options {
   };
 }
 
+const compressConfig = {
+  threshold: 1,
+  br: false as false,
+  gzip: {
+    flush: zlib.constants.Z_SYNC_FLUSH,
+  },
+  deflate: {
+    flush: zlib.constants.Z_SYNC_FLUSH,
+  },
+};
+
 // 静态资源应该放到 CDN 上
-export function lits({ ssl, apiApp, staticDir }: Options) {
-  if (started) {
-    throw new Error("Lits server has been started.");
-  }
-  started = true;
-  const app = new Koa();
+export function lits({ ssl, apiApp, devPort, staticDir }: Options) {
+  return new Promise<void>((resolve) => {
+    if (started) {
+      console.warn("Lits server has been started.");
+      return;
+    }
+    started = true;
+    const app = new Koa();
+    app.use(redirectHttps());
+    app.use(defense());
+    app.use(sslify());
+    app.use(cors());
+    app.use(mount("/api", apiApp));
+    app.use(mount("/deploy", createDeployApp(staticDir)));
+    app.use(compress(compressConfig));
+    app.use(koaStatic(staticDir));
 
-  const compressConfig = {
-    threshold: 1,
-    br: false as false,
-    gzip: {
-      flush: zlib.constants.Z_SYNC_FLUSH,
-    },
-    deflate: {
-      flush: zlib.constants.Z_SYNC_FLUSH,
-    },
-  };
-
-  app.use(redirectHttps());
-  app.use(defense());
-  app.use(sslify());
-  app.use(cors());
-  app.use(mount("/api", apiApp));
-  app.use(mount("/deploy", createDeployApp(staticDir)));
-  app.use(compress(compressConfig));
-  app.use(koaStatic(staticDir));
-
-  // Start server
-  if (isDev) {
-    http2.createSecureServer(ssl, app.callback()).listen(8000);
-    console.log("http2 server is running: https://localhost:8000");
-  } else {
-    http.createServer(app.callback()).listen(80);
-    http2.createSecureServer(ssl, app.callback()).listen(443);
-    console.log("http2 server is running.");
-  }
+    // Start server
+    if (isDev) {
+      http2.createSecureServer(ssl, app.callback()).listen(devPort, resolve);
+    } else {
+      let i = 0;
+      const meter = () => ++i === 2 && resolve();
+      http.createServer(app.callback()).listen(80, meter);
+      http2.createSecureServer(ssl, app.callback()).listen(443, meter);
+    }
+  });
 }
